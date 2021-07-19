ef853da4a24376f8805bb597f1b90d7906fc40bf
AlpakaGSB <86078160+AlpakaGSB@users.noreply.github.com>
2021-07-19 13:40:59 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,31 @@
+from tkinter import Tk
+
+
+root = Tk()
+
+# colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+LIGHTGREY = (100, 100, 100)
+SKYBLUE = (115, 215, 255)
+ORCHID = (218, 112, 214)
+DARKBLUE = (0, 0, 153)
+LIGHTPURPLE = (92, 107, 192)
+PEACH = (255, 165, 192)
+
+# game settings
+WIDTH = 1024  # root.winfo_screenwidth()
+HEIGHT = 768  # root.winfo_screenheight()
+FPS = 45
+TITLE = "aMAZEing"
+BACKGROUND_COLOR = SKYBLUE
+CELL_SIZE = 32
+CELL_12 = 12
+CELL_20 = 20
+CELL_32 = 32
+
+PLAYER_SPEED = 4
